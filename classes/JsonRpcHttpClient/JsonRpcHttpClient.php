e3a5677bb1e5365885cf9e2832ba9adec08009cc
王正东 <wangzhengdong@ecmoban.com>
2020-12-25 09:02:51 +0800

    扩展rpc-client，增加自定义请求授权

---
+++ classes/JsonRpcHttpClient/JsonRpcHttpClient.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Ecjia\System\Frameworks\JsonRpcHttpClient;
+
+use ecjia;
+use Ecjia\App\Rpc\Repositories\DefaultRpcAccountRepository;
+use Royalcms\Laravel\JsonRpcClient\BasicAuthentication;
+
+class JsonRpcHttpClient extends \Royalcms\Laravel\JsonRpcClient\JsonRpcHttpClient
+{
+    protected $account;
+
+    public function __construct()
+    {
+        $appid = ecjia::config('cashier_dscmall_rpc_appid');
+        $this->account = (new DefaultRpcAccountRepository())->getAccountByAppId($appid);
+
+        if (empty($this->account)) {
+            throw new \InvalidArgumentException('请求大商创RPC服务帐号未配置，请至设置中选择！');
+        }
+    }
+
+    /**
+     * @return string
+     */
+    protected function getUri()
+    {
+        return $this->account->callback_url;
+    }
+
+    /**
+     * 自定义请求Header信息
+     * @return string[]
+     */
+    protected function getHeaders()
+    {
+        return (new BasicAuthentication($this->account->appid, $this->account->appsecret))->getAuthorizationHeaders();
+    }
+
+
+}
