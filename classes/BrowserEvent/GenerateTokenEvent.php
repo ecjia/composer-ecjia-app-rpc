cc59239e5a26674ac9cc8e8be2079277dd4cf726
王正东 <wangzhengdong@ecmoban.com>
2021-01-14 17:42:29 +0800

    优化模板中JS事件代码

--- classes/BrowserEvent/GenerateTokenEvent.php
+++ classes/BrowserEvent/GenerateTokenEvent.php
@@ -5,7 +5,6 @@ namespace Ecjia\App\Rpc\BrowserEvent;
 
 
 use Ecjia\Component\BrowserEvent\BrowserEventInterface;
-use RC_Uri;
 
 class GenerateTokenEvent implements BrowserEventInterface
 {
@@ -17,78 +16,23 @@ class GenerateTokenEvent implements BrowserEventInterface
 
     public function __invoke()
     {
-        $validate_length = 4;
-        $validate_url    = RC_Uri::url('captcha/admin_captcha/check_validate');
-
         return <<<JS
 (function () {
-    //打开验证码显示
-    let captcha_element = $('input[name="captcha"]');
-    let captcha_popover = $('.captcha-popover');
-    let captcha_popover_img = captcha_popover.find('img');
-    let validate_length = {$validate_length};
-    let validate_url = "{$validate_url}";
-    
-    captcha_element.popover({
-        html: true,
-        animation: false,
-        trigger: 'manual',
-        content: function() {
-            let width = captcha_popover_img.width() + 20;
-            let height = captcha_popover_img.height() + 20;
-            return captcha_popover.clone().css({width : width, height: height, position : 'relative', zIndex : '9999'});
-        }
-    });
-    
-    captcha_element.keyup(function(event){
-        if(event.keyCode === 27 || event.keyCode === 13){
-            $('.popover').remove();
-            $(this).blur();
-        }
-            
-        if(event.keyCode === 13){
-            return;
-        }
-        
+    $('.toggle_view').on('click', function (event) {
+        event.preventDefault();
         let that = $(this);
-        let formRow = that.parents('.formRow');
-        that.val(that.val().toUpperCase());
-        if (that.val().length === validate_length) {
-            $.post(
-                validate_url, 
-                {
-                    'captcha': that.val()
-                }, 
-                function(data) {
-                    if (data.state === 'success') {
-                        formRow.hasClass('f_success') ? formRow.removeClass('f_error') : formRow.removeClass('f_error').addClass('f_success');
-                        $('.popover').remove();
-                    } else {
-                        formRow.hasClass('f_error') ? formRow.removeClass('f_success') : formRow.removeClass('f_success').addClass('f_error');
-                    }
-                });
+        let url = that.attr('href');
+        let val = that.attr('data-val') || 'allow';
+        let data = {
+            check: val
         }
+        $.post(url, data, function (data) {
+            if (data.state === 'success') {
+                $('.generate_token').val(data.token);
+            }
+            ecjia.admin.showmessage(data);
+        }, 'json');
     });
-    
-    captcha_element.focus(function () {
-        if (!$('captcha-popover .popover').text()) {
-            $(this).popover('show');
-        }
-    });
-    
-    //关闭验证码窗口
-    $(document).on('click', '.close', function(){
-        console.log('click');
-        $('.popover').remove();
-    });
-    
-    //更换验证码
-    $(document).on('click', '.popover img', function() {
-        let src = $(this).attr('src') + Math.random();
-        $(this).attr('src', src);
-        captcha_popover_img.attr('src', src);
-    });
-    
 })();
 JS;
 
